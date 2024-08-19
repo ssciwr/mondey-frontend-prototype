51af88b3b960eb655bb75e6b4006ee16847bd613
Liam Keegan <liam@keegan.ch>
2024-08-19 10:41:53 +0200

    use flowbite in tailwind config, add WIP banner to front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { Banner } from 'flowbite-svelte';
+</script>
+
+<div class="p-8">
+    <Banner>
+        This is a work-in-progress prototype frontend for the MONDEY website
+    </Banner>
+</div>
