39173e8579c1b4cc4c16ce2ab66a20784253edd8
Harald Mack <harald.mack@protonmail.com>
2024-08-20 14:23:34 +0200

    add content page

---
+++ src/lib/components/AbstractContent.svelte
@@ -0,0 +1,108 @@
+<script>
+	import {
+		Avatar,
+		BottomNav,
+		BottomNavItem,
+		DarkMode,
+		Navbar,
+		NavBrand,
+		NavHamburger,
+		NavLi,
+		NavUl,
+		Tooltip
+	} from 'flowbite-svelte';
+
+	import {
+		CaretLeftSolid,
+		CaretRightSolid,
+		LightbulbSolid,
+		MoonSolid,
+		SunSolid
+	} from 'flowbite-svelte-icons';
+
+	import logo_dark from '../assets/mondey_dark.svg';
+	import logo_light from '../assets/mondey_light.svg';
+
+	// variables for showing or hiding elements
+	export let showBottomNavbar = true;
+
+	// variables for communication upon hitting the page
+	// FIXME: these are placeholders and must later be changed using contexts or other better mechanisms for inter component comunication
+	export let lastpage = '/';
+	export let nextpage = '/';
+	export let infopage = '/';
+
+	export let name = 'Registrieren';
+
+	const basicStyle = {
+		basicBtnForm: 'rounded-lg text-xl p-2',
+		basicColorBehavior:
+			'text-gray-500 dark:text-gray-400 hover:text-primary-800 dark:hover:text-primary-500 group-hover:text-primary-800 dark:group-hover:text-primary-500'
+	};
+
+	const topNavStyle = {
+		topBarBtnStyle: basicStyle.basicColorBehavior + basicStyle.basicBtnForm,
+		navULStyle:
+			'flex flex-col p-4 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:text-lg md:font-medium'
+	};
+
+	const bottomNavStyle = {
+		bottomBarBtnStyle: 'w-8 h-8 mb-1' + basicStyle.basicColorBehavior,
+		bottomNavStyleInner: 'grid-cols-3'
+	};
+
+	const avatar_style = {
+		outer: 'flex items-center space-x-4 rtl:space-x-reverse',
+		avatar: 'flex items-center space-x-4 rtl:space-x-reverse',
+		inner: 'space-y-1' + basicStyle.basicColorBehavior
+	};
+	const contentContainerStyle = 'mx-auto max-w-6xl p-4';
+</script>
+
+<!-- Top element: basic navigation-->
+<Navbar>
+	<NavBrand href="/">
+		<img src={logo_light} class="block h-16 dark:hidden" alt="MONDEY Logo" />
+		<img src={logo_dark} class="hidden h-16 dark:block" alt="MONDEY Logo" />
+	</NavBrand>
+	<NavHamburger />
+	<NavUl ulClass={topNavStyle.navULStyle}>
+		<NavLi href="/" active={true}>Home</NavLi>
+		<NavLi href="/">Aktuelles</NavLi>
+		<NavLi href="/">Downloads</NavLi>
+		<NavLi href="/">Kontakt</NavLi>
+		<DarkMode class={topNavStyle.topBarBtnStyle}>
+			<MoonSolid slot="darkIcon" />
+			<SunSolid slot="lightIcon" />
+		</DarkMode>
+		<Tooltip arrow={false}>Darkmode ein- oder ausschalten</Tooltip>
+	</NavUl>
+	<div class={avatar_style.outer}>
+		<Avatar class={avatar_style.avatar} rounded border />
+		<div class={avatar_style.inner}>{name}</div>
+		<Tooltip>Registrieren oder einloggen</Tooltip>
+	</div>
+</Navbar>
+
+<!--Page content goes here-->
+<div class={contentContainerStyle}>
+	<slot></slot>
+</div>
+
+{#if showBottomNavbar}
+	<!-- bottom element: back, info and next buttons-->
+	<BottomNav position="absolute" classInner={bottomNavStyle.bottomNavStyleInner}>
+		<BottomNavItem href={lastpage} btnName="Zurück" btnClass={basicStyle.basicColorBehavior}>
+			<CaretLeftSolid class={bottomNavStyle.bottomBarBtnStyle} />
+			<Tooltip arrow={false}>Zur letzten Seite</Tooltip>
+		</BottomNavItem>
+		<BottomNavItem href={infopage} btnName="Hilfe" btnClass={basicStyle.basicColorBehavior}>
+			<LightbulbSolid class={bottomNavStyle.bottomBarBtnStyle} />
+			<Tooltip arrow={false}>Hilfe</Tooltip>
+		</BottomNavItem>
+		<BottomNavItem href={nextpage} btnName="Weiter" btnClass={basicStyle.basicColorBehavior}>
+			<CaretRightSolid class={bottomNavStyle.bottomBarBtnStyle} />
+			<Tooltip arrow={false}>Zur nächsten Seite</Tooltip>
+		</BottomNavItem>
+	</BottomNav>
+{/if}
---
+++ src/lib/components/Frontpage.svelte
@@ -0,0 +1,61 @@
+<script>
+	import { Button, Card, Gallery } from 'flowbite-svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+
+	// Define the styles for the header, paragraph, arrow, and button
+	const headerStyle = 'mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white';
+	const paragraphStyle = 'mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight';
+	const arrowStyle = 'ms-2 h-6 w-6 text-white';
+	const buttonStyle = 'w-fit';
+	const galleryStyle = 'grid-cols-2 gap-y-12';
+	const cardStyle = 'p-6 m-4 w-full';
+</script>
+
+<!-- This is a dummy landing page that is only there to check that the AbstractComponent works as it should-->
+<Gallery class={galleryStyle}>
+	<Card class={cardStyle}>
+		<h5 class={headerStyle}>Was ist Mondey?</h5>
+		<p class={paragraphStyle}>
+			<i>Mondey</i> ist ein wissentschaftlich geprüftes Programm zure Dokumentation der Entwicklung von
+			Kindern bis 6 Jahren.
+		</p>
+		<Button class={buttonStyle}>Mehr Info <ArrowRightOutline class={arrowStyle} /></Button>
+	</Card>
+
+	<Card class={cardStyle}>
+		<h5 class={headerStyle}>Wozu ist Mondey gut?</h5>
+		<p class={paragraphStyle}>
+			Anhand ihrer Bewertungen der Fähigkeiten des Kindes erhalten sie Feedback zum
+			Entwicklungsstand des Kindes und können so frühzeitig Fördermaßnahmen einleiten. Dies folgt
+			einem übersichtlichen Ampelsystem.
+		</p>
+		<Button class={buttonStyle}>Mehr Info <ArrowRightOutline class={arrowStyle} /></Button>
+	</Card>
+
+	<Card class={cardStyle}>
+		<h5 class={headerStyle}>Was umfasst Mondey?</h5>
+		<p class={paragraphStyle}>
+			<i>Mondey</i> umfasst unterschiedliche Entwicklungsbereiche wie von Kindern im Alter von 0 bis
+			6 Jahren. Dazu gehören unter anderem Grob-und feinmotorik, Wahrnehmung, Denkne, Sprache und Soziale
+			Beziehungen.
+		</p>
+		<Button class={buttonStyle}>Mehr Info <ArrowRightOutline class={arrowStyle} /></Button>
+	</Card>
+
+	<Card class={cardStyle}>
+		<h5 class={headerStyle}>Wie funktioniert Mondey?</h5>
+		<p class={paragraphStyle}>
+			Sie bewerten wie gut das Kind bestimmte Alltagshandlungen durchführen kann mit Hilfe einer
+			Liste von Fragen.
+		</p>
+		<Button class={buttonStyle}>Mehr Info <ArrowRightOutline class={arrowStyle} /></Button>
+	</Card>
+
+	<Card class={cardStyle}>
+		<h5 class={headerStyle}>Wo fange ich an?</h5>
+		<p class={paragraphStyle}>
+			Um zu beginnen, müssen sie sich registrieren und ein Profil für ihr Kind anlegen.
+		</p>
+		<Button class={buttonStyle}>Los geht's <ArrowRightOutline class={arrowStyle} /></Button>
+	</Card>
+</Gallery>
