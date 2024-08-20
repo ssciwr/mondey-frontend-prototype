eb8b8bf22b1becc5c3476b5886538deddc5e153a
Harald Mack <harald.mack@protonmail.com>
2024-08-20 16:21:15 +0200

    mess with styling

--- src/lib/components/AbstractDropdown.svelte
+++ src/lib/components/AbstractDropdown.svelte
@@ -8,16 +8,16 @@
 	// @ts-ignore
 	export let dropdownData = [];
 	export let heading = 'dummy dropdown data';
-	const textColors = 'text-gray-700 dark:text-gray-400';
+	const textColors = 'text-gray-700 dark:text-gray-400 ';
 	const basicColorBehavior =
 		textColors +
-		'hover:text-primary-800 dark:hover:text-primary-500 group-hover:text-primary-800 dark:group-hover:text-primary-500';
-	const headerStyle = 'mb-2 text-2xl font-bold tracking-tight' + textColors;
-	const paragraphStyle = 'mb-3 font-normal leading-tight' + textColors;
-	const symbolStyle = 'h-6 w-6' + textColors;
+		'hover:text-primary-800 dark:hover:text-primary-500 group-hover:text-primary-800 dark:group-hover:text-primary-500 ';
+	const headerStyle = 'mb-2 text-2xl font-bold tracking-tight ' + textColors;
+	const paragraphStyle = 'mb-3 font-normal leading-tight ' + textColors;
+	const symbolStyle = 'h-6 w-6 ' + textColors;
 	const labelStyle = 'mb-2 text-lg ' + textColors;
-	const labelContainerStyle = 'flex items-center space-x-2';
-	const buttonStyle = '' + basicColorBehavior;
+	const labelContainerStyle = 'flex items-center space-x-2 ';
+	const buttonStyle = ' ';
 </script>
 
 <div class="items-center space-x-4 rtl:space-x-reverse">
