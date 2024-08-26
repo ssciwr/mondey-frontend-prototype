365b19f3ed5f098d4f01052a3c1eab7f2cecdf63
Harald Mack <harald.mack@protonmail.com>
2024-08-26 11:59:31 +0200

    update registration page with dummy data for different roles

--- src/routes/registerUser/userDataInput/+page.svelte
+++ src/routes/registerUser/userDataInput/+page.svelte
@@ -4,21 +4,27 @@
 	import AbstractRegistrationForm from '$lib/components/DataInput/AbstractRegistrationForm.svelte';
 	import { Input } from 'flowbite-svelte';
 
-	function range(size, startAt = 0, step = 1) {
+	function intervalRange(size, startAt = 0, step = 1) {
+		return [...Array(size).keys()].map(
+			(i) => String(i * step + startAt) + '-' + String((i + 1) * step + startAt)
+		);
+	}
+
+	function numericalRange(size, startAt = 0, step = 1) {
 		return [...Array(size).keys()].map((i) => i * step + startAt);
 	}
 
-	export let role = 'Benutzer/in';
+	export let role = 'Admin';
 
 	const heading = 'Benutzerdaten eingeben';
 	const description =
 		'Bitte vervollständigen sie ihre Daten um die Registrierung abzuschließen. Wählen sie dazu das jeweils zutreffende aus der jeweiligen Liste aus';
-	const props = [
+	const user_props = [
 		{
 			itemComponent: AbstractDropdownItem,
 			componentProps: {
 				name: 'Geburtsjahr',
-				items: range(100, 1920),
+				items: numericalRange(100, 1920),
 				about: 'Wählen sie ihr Geburtsjahr aus',
 				selected: 1988
 			}
@@ -54,20 +60,20 @@
 			itemComponent: AbstractDropdownItem,
 			componentProps: {
 				name: 'Arbeitszeit/Woche',
-				items: range(13, 0, 5),
+				items: intervalRange(13, 0, 5),
 				about:
 					'Wählen sie ihre Arbeitszeit pro Woche aus. Wählen sie die Zahl, die dem tatsächlichen Wert am nächsten kommt.',
-				selected: 40
+				selected: '35-40'
 			}
 		},
 		{
 			itemComponent: AbstractDropdownItem,
 			componentProps: {
 				name: 'Familieneinkommen/Jahr',
-				items: range(23, 0, 5000),
+				items: intervalRange(23, 0, 5000),
 				about:
 					'Wählen sie ihre Jahreseinkommen aus. Wählen sie die Zahl, die dem tatsächlichen Wert am nächsten kommt.',
-				selected: 50000
+				selected: '50000-55000'
 			}
 		},
 
@@ -82,14 +88,40 @@
 		}
 	];
 
-	if (role == 'Forscher/in') {
+	let props = user_props;
+
+	$: if (role === 'Forscher/in') {
+		props = user_props;
 		props.push({
 			itemComponent: AbstractDropdownItem,
 			componentProps: {
 				name: 'Forschungsbereich',
-				items: ['Bereich 1', 'Bereich 2', 'Bereich 3'],
+				items: ['Psychologie', 'Medizin', 'Physik'],
 				about: 'Wählen sie ihren Forschungsbereich aus',
-				selected: 'Bereich 1'
+				selected: 'Psychologie'
+			}
+		});
+
+		props.push({
+			itemComponent: Input,
+			componentProps: {
+				name: 'Institution',
+				type: 'text',
+				about: 'Geben sie an an welcher Forschungsinstitution sie tätig sind',
+				placeholder: 'Forschungsinstitution'
+			}
+		});
+	}
+
+	$: if (role == 'Admin') {
+		props = user_props;
+		props.push({
+			itemComponent: Input,
+			componentProps: {
+				name: 'Institution',
+				type: 'text',
+				about: 'Geben sie ihr Admin Token ein',
+				placeholder: 'Admin Token'
 			}
 		});
 	}
