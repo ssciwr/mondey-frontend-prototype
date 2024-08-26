e6961c9812096a48de06c1963785b22533eb1c44
Harald Mack <harald.mack@protonmail.com>
2024-08-26 08:22:35 +0200

    add userdatainput, add user data

--- src/lib/components/DataInput/AbstractRegistrationForm.svelte
+++ src/lib/components/DataInput/AbstractRegistrationForm.svelte
@@ -2,19 +2,17 @@
 	import { Button, Card, Heading, Input, Label } from 'flowbite-svelte';
 
 	export let data;
+	export let heading = '';
+	export let buttonLabel = '';
 </script>
 
 <Card>
+	<Heading tag="h3">{heading}</Heading>
 	<form>
-		<Heading tag="h3">Registrieren sie sich als neuer Nutzer</Heading>
-		<Label>Benutzername</Label>
-		<Input type="text" label="Username" placeholder="Bestimmen sie eine beliebigen Benutzernamen" />
-		<Label>Email (optional)</Label>
-		<Input type="text" label="Email" placeholder="Email" />
-		<Label>Passwort</Label>
-		<Input type="text" label="Password" placeholder="Passwort" />
-		<Label>Passwort bestätigen</Label>
-		<Input type="text" label="Confirm Password" placeholder="Passwort bestätigen" />
-		<Button type="submit" class="w-full">Weiter</Button>
+		{#each data as { label, type, placeholder }}
+			<Label>{label}</Label>
+			<Input {type} {label} {placeholder} />
+		{/each}
 	</form>
+	<Button type="submit" href="/registerUser/userDataInput" class="w-full">{buttonLabel}</Button>
 </Card>
--- src/routes/registerUser/+page.svelte
+++ src/routes/registerUser/+page.svelte
@@ -1,6 +1,31 @@
 <script>
 	import AbstractRegistrationForm from '$lib/components/DataInput/AbstractRegistrationForm.svelte';
-	export let role = 'surveyor'; // researcher, admin, surveyor
+	const data = [
+		{
+			label: 'Benutzername',
+			type: 'text',
+			placeholder: 'Wählen sie einen beliebigen Benutzernamen'
+		},
+		{
+			label: 'E-Mail (optional)',
+			type: 'text',
+			placeholder: 'E-Mail'
+		},
+		{
+			label: 'Passwort',
+			type: 'text',
+			placeholder: 'Passwort'
+		},
+		{
+			label: 'Passwort wiederholen',
+			type: 'text',
+			placeholder: 'Passwort wiederholen'
+		}
+	];
+	const heading = 'Als neuer Benutzer registrieren';
+	const buttonLabel = 'Weiter';
 </script>
 
-<AbstractRegistrationForm />
+<div class="flex">
+	<AbstractRegistrationForm {data} {heading} {buttonLabel} />
+</div>
