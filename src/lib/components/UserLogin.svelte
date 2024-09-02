dc5ef3482fa6d1f3795306e93f19b357ad162914
Harald Mack <harald.mack@protonmail.com>
2024-09-02 09:14:22 +0200

    reorganize user login page

--- src/lib/components/UserLogin.svelte
+++ src/lib/components/UserLogin.svelte
@@ -1,52 +1,26 @@
 <script>
-	import AbstractRegistrationForm from '$lib/components/DataInput/AbstractRegistrationForm.svelte';
-	import UserLoginUtil from '$lib/components/UserLoginUtil.svelte';
-	import { Input } from 'flowbite-svelte';
-
-	export let role = 'user';
+	import Input from '$lib/components/DataInput/Input.svelte';
+	import { Card, Heading } from 'flowbite-svelte';
+	import UserLoginUtil from './UserLoginUtil.svelte';
 
 	let data = [
 		{
-			itemComponent: Input,
-			componentProps: {
-				type: 'text',
-				name: 'Benutzerkennung',
-				placeholder: 'Benutzerkennung',
-				label: 'Benutzerkennung',
-				value: '',
-				required: true
-			}
+			type: 'text',
+			name: 'Benutzerkennung',
+			placeholder: 'Benutzerkennung',
+			label: 'Benutzerkennung',
+			value: '',
+			required: true
 		},
 		{
-			itemComponent: Input,
-			componentProps: {
-				type: 'password',
-				name: 'Passwort',
-				placeholder: 'Passwort',
-				label: 'Passwort',
-				value: '',
-				required: true
-			}
-		},
-		{
-			itemComponent: UserLoginUtil,
-			componentProps: {}
+			type: 'password',
+			name: 'Passwort',
+			placeholder: 'Passwort',
+			label: 'Passwort',
+			value: '',
+			required: true
 		}
 	];
-
-	$: if (role === 'admin') {
-		data.push({
-			itemComponent: Input,
-			componentProps: {
-				type: 'text',
-				name: 'Projektcode',
-				placeholder: 'Projektcode',
-				label: 'Projektcode',
-				value: '',
-				required: true
-			}
-		});
-	}
 
 	const buttons = [
 		{
@@ -54,15 +28,32 @@
 			href: '/'
 		}
 	];
+
+	const heading = 'Einloggen';
 </script>
 
-<div class="container m-1 mx-auto w-full max-w-md space-y-6">
-	<AbstractRegistrationForm heading={'Einloggen'} {buttons} props={data} />
-	<span class="text-gray-700 dark:text-gray-400">Not registered?</span>
+<Card class="container m-1 mx-auto w-full max-w-xl">
+	{#if heading}
+		<Heading
+			tag="h3"
+			class="m-1 mb-3 p-1 text-center font-bold tracking-tight text-gray-700 dark:text-gray-400"
+			>{heading}</Heading
+		>
+	{/if}
+
+	<form class="m-1 m-3 mx-auto w-full flex-col space-y-6">
+		{#each data as element, i}
+			<Input {element} />
+		{/each}
+	</form>
+
+	<UserLoginUtil cls="p-6 mb-3" />
+
+	<span class="container mx-auto w-full text-gray-700 dark:text-gray-400">Not registered?</span>
 	<a
 		href="/userLand/userRegistration"
 		class="text-primary-700 hover:underline dark:text-primary-500"
 	>
 		Create account
 	</a>
-</div>
+</Card>
--- src/lib/components/UserLoginUtil.svelte
+++ src/lib/components/UserLoginUtil.svelte
@@ -1,8 +1,9 @@
-<script>
+<script lang="ts">
 	import { Checkbox } from 'flowbite-svelte';
+	export let cls: String = '';
 </script>
 
-<div class="container mx-auto flex w-full max-w-md items-start">
+<div class={'container mx-auto flex w-full max-w-md items-start' + cls}>
 	<Checkbox>Remember me</Checkbox>
 	<a href="/" class="ms-auto text-primary-700 hover:underline dark:text-primary-500">
 		Lost password?
