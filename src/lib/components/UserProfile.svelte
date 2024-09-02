fda85f6aebadf483372ca12b41c60283ba47f39c
Harald Mack <harald.mack@protonmail.com>
2024-09-02 14:30:22 +0200

    add userprofile popover

---
+++ src/lib/components/UserProfile.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { users } from '$lib/stores/userStore';
+	import { Button, Heading, Listgroup, Popover } from 'flowbite-svelte';
+	import { onDestroy } from 'svelte';
+
+	export let triggeredBy = '';
+	let loggedIn: string | null = users.get()['loggedIn'];
+	let userData: UserData | null = loggedIn ? users.fetch(loggedIn) : null;
+
+	const unsubscribe = users.subscribe((currentUserData) => {
+		loggedIn = currentUserData['loggedIn'];
+
+		if (loggedIn && loggedIn !== null) {
+			userData = users.fetch(loggedIn);
+		}
+	});
+
+	function logout() {
+		users.get()['loggedIn'] = null;
+		userData = null;
+	}
+
+	onDestroy(unsubscribe);
+</script>
+
+<Popover {triggeredBy} class="text-gray-700 dark:text-gray-400">
+	{#if userData !== null}
+		<div class="mx-auto mb-6 flex flex-col items-center justify-center space-y-6">
+			<Listgroup
+				active
+				items={[
+					{ name: 'Übersicht', href: '/childrengallery', current: true },
+					{ name: 'Profil', href: '/' }
+				]}
+				let:item
+			>
+				{item.name}
+			</Listgroup>
+			<Button href="/" on:click={logout}>Logout</Button>
+		</div>
+	{:else}
+		<div class="mx-auto mb-6 flex flex-col items-center justify-center space-y-6">
+			<Heading tag="h3">Willkommen!</Heading>
+			<Button href="/userLand/userLogin">Einloggen oder Registrieren</Button>
+		</div>
+	{/if}
+</Popover>
