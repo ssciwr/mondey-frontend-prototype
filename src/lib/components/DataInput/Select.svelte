a28ffbaf734b25de430fef68316b5f8d64f98c14
Harald Mack <harald.mack@protonmail.com>
2024-09-05 13:55:15 +0200

    rename dropdown component

---
+++ src/lib/components/DataInput/Select.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Label, Select } from 'flowbite-svelte';
+	export let value: string;
+</script>
+
+{#if $$props.label}
+	<Label class="font-semibold text-gray-700 dark:text-gray-400">{$$props.label}</Label>
+{/if}
+
+<div>
+	<Select
+		size="lg"
+		class="mb-3 font-normal leading-tight text-gray-700 dark:text-gray-400"
+		items={$$props.items.map((v) => {
+			return {
+				name: String(v),
+				value: v
+			};
+		})}
+		placeholder={$$props.about}
+		bind:value
+	/>
+</div>
