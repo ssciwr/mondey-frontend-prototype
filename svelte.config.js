076d558c3112842240ecd013f0fc27ba17f65deb
Liam Keegan <liam@keegan.ch>
2024-09-09 10:14:45 +0200

    fix svelteconfig json

--- svelte.config.js
+++ svelte.config.js
@@ -16,10 +16,10 @@ const config = {
 			fallback: 'index.html',
 			precompress: false,
 			strict: true,
-			paths: {
-				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-			}
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
