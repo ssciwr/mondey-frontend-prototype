48aa1b5db69c99835186a62e37c05df656386231
Liam Keegan <liam@keegan.ch>
2024-09-09 10:12:12 +0200

    explicitly use BASE_PATH env var in svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,10 @@ const config = {
 			assets: 'build',
 			fallback: 'index.html',
 			precompress: false,
-			strict: true
+			strict: true,
+			paths: {
+				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			}
 		})
 	}
 };
