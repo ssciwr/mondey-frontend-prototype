e0e9ac05e2c9d5a6d751c5408cc223344a98f008
Liam Keegan <liam@keegan.ch>
2024-09-12 17:07:56 +0200

    use Annotated and Depends to get session inside a request

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,21 +2,26 @@ from __future__ import annotations
 
 from sqlmodel import Field, SQLModel, Relationship
 
+
 class MilestoneGroupBase(SQLModel):
     title: str
     image: str
     description: str
     order: int
 
+
 class MilestoneGroup(MilestoneGroupBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     milestones: list["Milestone"] = Relationship(back_populates="group")
 
-class MilestoneGroupCreate(MilestoneGroupBase, table=True):
+
+class MilestoneGroupCreate(MilestoneGroupBase):
     pass
+
 
 class MilestoneGroupPublic(MilestoneGroupBase):
     id: int
+
 
 class MilestoneBase(SQLModel):
     group_id: int | None = Field(default=None, foreign_key="milestonegroup.id")
@@ -26,12 +31,15 @@ class MilestoneBase(SQLModel):
     description: str
     help: str
 
+
 class Milestone(MilestoneBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     group: MilestoneGroup = Relationship(back_populates="milestones")
 
+
 class MilestoneCreate(MilestoneBase):
     pass
+
 
 class MilestonePublic(MilestoneBase):
     id: int
