209a026e70ba61b5c34238e45f43e97e1b52b01d
Harald Mack <harald.mack@protonmail.com>
2024-09-13 11:09:08 +0200

    add link to the tablecell

--- frontend/src/lib/components/DataDisplay/TableElements/TableCell.svelte
+++ frontend/src/lib/components/DataDisplay/TableElements/TableCell.svelte
@@ -1,14 +1,15 @@
-<script>
+<script lang="ts">
 	import { TableBodyCell } from 'flowbite-svelte';
 
 	export let value;
 	export let statusIndicator;
 	export let statusColumns;
 	export let key;
+	export let href: string = null;
 </script>
 
 {#if statusColumns.includes(key)}
-	<TableBodyCell class={statusIndicator[value]}>{value}</TableBodyCell>
+	<TableBodyCell class={statusIndicator[value]}><a {href}>{value}</a></TableBodyCell>
 {:else}
-	<TableBodyCell>{value}</TableBodyCell>
+	<TableBodyCell><a {href}>{value}</a></TableBodyCell>
 {/if}
