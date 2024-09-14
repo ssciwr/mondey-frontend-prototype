3f24aa626f7fc211ff66e776c96c288c79dc70e0
Harald Mack <harald.mack@protonmail.com>
2024-09-14 09:23:46 +0200

    refactor link building

--- frontend/src/routes/childLand/[userID]/[childID]/+page.svelte
+++ frontend/src/routes/childLand/[userID]/[childID]/+page.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
 	import TableDisplay from '$lib/components/DataDisplay/TableDisplay.svelte';
+	import { children } from '$lib/stores/childrenStore';
+	import { onMount } from 'svelte';
 
 	// this will be passed from the backend eventually
 	export let data_to_display = $$props.data.observationData.summary;
 	// process data to add links: This could be done in the backend or stored accordingly
 	// this should not live in the data_to_display datastructure that will be obtained from the backend
 	const celllinks = data_to_display.map((element) => {
-		let link = [];
-		for (let date of Object.keys(element).filter((e) => e != 'name')) {
-			link.push('/milestone'); // TODO: this currently only links to a dummy milestone page
-		}
-		return link;
+		return Object.keys(element).map((k) => {
+			if (k === 'name') {
+				return null;
+			} else {
+				return '/milestone'; // TODO: this would lead to a page where the actual data is loaded for the survey
+			}
+		});
 	});
 
 	const headerlinks = Object.keys(data_to_display[0]).map((k) => {
@@ -33,6 +37,10 @@
 
 	const caption =
 		'Here you see an overview of all completed and outstanding milestones for the current inventory.';
+
+	onMount(() => {
+		children.load();
+	});
 </script>
 
 <TableDisplay
