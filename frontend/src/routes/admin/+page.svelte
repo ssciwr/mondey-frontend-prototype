09aecd9d27a80070a5c055570b76d681711cf530
Liam Keegan <liam@keegan.ch>
2024-09-20 16:58:49 +0200

    check if admin user is logged in on page load

--- frontend/src/routes/admin/+page.svelte
+++ frontend/src/routes/admin/+page.svelte
@@ -3,10 +3,22 @@
     import MilestoneGroup from '$lib/components/Admin/MilestoneGroup.svelte';
     import Login from '$lib/components/Admin/Login.svelte'
 
+    async function updateIsLoggedIn(){
+        const res = await fetch("http://localhost:8000/users/me", {
+            method: 'GET',
+            credentials: 'include',
+        });
+        if (res.status === 200) {
+            isLoggedIn.set(true);
+        }
+    }
+    updateIsLoggedIn();
+
     let isLoggedInValue: boolean;
     const unsubscribe = isLoggedIn.subscribe((value) => {
         isLoggedInValue = value;
     });
+
 </script>
 
 <div class="flex flex-col justify-center items-center">
