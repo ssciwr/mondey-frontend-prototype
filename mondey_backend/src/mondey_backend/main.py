612bdb1397946367c2f3213a500f8ac0ec57dd79
Liam Keegan <liam@keegan.ch>
2024-09-22 20:23:38 +0200

    Extend admin interface
    - admin frontend
      - languages can be added, deleted
        - use ISO6391 library to get list of possible language codes and translate codes to native names
      - milestonegroups can be added, edited, deleted
      - milestone group images can be uploaded
      - add lib/admin with functions to interact with API
      - add adminStore with milestones
        - also includes lang_id and isLoggedIn temporarily, probably these should be refactored into main site
    - backend
      - add Language model
        - use this id instead of 2-character string to identify language
        - use `(group_id,lang_id)` as primary key for MilestoneGroupText, remove separate primary key
        - add endpoints to get, create and delete languages

--- mondey_backend/src/mondey_backend/main.py
+++ mondey_backend/src/mondey_backend/main.py
@@ -38,7 +38,7 @@ app.mount(
 if app_settings.ENABLE_CORS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:4173", "http://localhost:5173"],
+        allow_origins=["http://localhost:5173"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
