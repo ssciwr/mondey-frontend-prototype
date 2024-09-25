f70059f18932d3ed75d839ed34eb9537f3b29d14
Harald Mack <harald.mack@protonmail.com>
2024-09-25 17:25:48 +0200

    correct data in userlogin page

--- frontend/src/routes/userLand/userLogin/+page.svelte
+++ frontend/src/routes/userLand/userLogin/+page.svelte
@@ -1,20 +1,39 @@
 <script>
 	import UserLogin from '$lib/components/UserLogin.svelte';
+	import { Input, Select } from 'flowbite-svelte';
 
 	const data = [
 		{
-			type: 'text',
-			name: 'Benutzerkennung',
-			placeholder: 'Benutzerkennung',
-			label: 'Benutzerkennung',
-			required: true
+			component: Input,
+			value: null,
+			props: {
+				label: 'Benutzerkennung',
+				type: 'text',
+				placeholder: 'Benutzerkennung',
+				required: true
+			}
 		},
 		{
-			type: 'password',
-			name: 'Passwort',
-			placeholder: 'Passwort',
-			label: 'Passwort',
-			required: true
+			component: Input,
+			value: null,
+			props: {
+				label: 'Passwort',
+				type: 'password',
+				placeholder: 'Passwort',
+				required: true
+			}
+		},
+		{
+			component: Select,
+			value: null,
+			props: {
+				label: 'Rolle',
+				items: ['Beobachter', 'Wissenschaftler', 'Admin'].map((v) => {
+					return { name: String(v), value: v };
+				}),
+				placeholder: 'Bitte auswählen',
+				required: true
+			}
 		}
 	];
 </script>
