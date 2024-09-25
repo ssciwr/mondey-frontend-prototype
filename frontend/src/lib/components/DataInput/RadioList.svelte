908718a07e461cfb9245d600cb532d1a4749e60c
Harald Mack <harald.mack@protonmail.com>
2024-09-25 14:36:23 +0200

    add RadioButton

--- frontend/src/lib/components/DataInput/RadioList.svelte
+++ frontend/src/lib/components/DataInput/RadioList.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
-	import { Radio } from 'flowbite-svelte';
+	import { Radio, RadioButton } from 'flowbite-svelte';
+
 	export let value: any;
+	let windowWidth = 1920;
+	$: smallScreen = windowWidth < 800;
 </script>
 
+<svelte:window bind:innerWidth={windowWidth} />
+
 <div class="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-	{#each $$props.items as item, index}
-		<Radio class="mx-2" bind:group={value} value={item.value}>{item.label}</Radio>
-	{/each}
+	{#if smallScreen}
+		{#each $$props.items as item, index}
+			<RadioButton class="mx-2" bind:group={value} value={item.value}>{item.label}</RadioButton>
+		{/each}
+	{:else}
+		{#each $$props.items as item, index}
+			<Radio class="mx-2" bind:group={value} value={item.value}>{item.label}</Radio>
+		{/each}
+	{/if}
 </div>
