ff85d0dbaf42202d4fab359b1eac38672943e106
Harald Mack <harald.mack@protonmail.com>
2024-09-26 11:24:08 +0200

    more restyling

--- frontend/src/lib/components/Breadcrumbs.svelte
+++ frontend/src/lib/components/Breadcrumbs.svelte
@@ -5,10 +5,16 @@
 </script>
 
 <Breadcrumb
-	olClass="inline-flex items-center space-x-1 rtl:space-x-reverse md:space-x-3 rtl:space-x-reverse flex-wrap"
-	navClass="m-2"
+	olClass="inline-flex items-center space-x-1 rtl:space-x-reverse md:space-x-3 rtl:space-x-reverse  flex-wrap "
+	navClass="m-4"
+	solidClass="rounded-tl rounded-tr flex px-5 py-3 text-gray-700 border border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 "
+	solid
 >
 	{#each data as item}
-		<BreadcrumbItem href={item.href}>{item.label}</BreadcrumbItem>
+		<BreadcrumbItem
+			href={item.href}
+			linkClass="ms-1 text-md font-medium text-gray-700 hover:text-primary-700 md:ms-2 dark:text-gray-400 dark:hover:text-white"
+			>{item.label}</BreadcrumbItem
+		>
 	{/each}
 </Breadcrumb>
