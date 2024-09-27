3787ab1944aaf40d2e4b90be47e631656dcda416
Harald Mack <harald.mack@protonmail.com>
2024-09-27 14:24:13 +0200

    add the required elements to milestonegroup data again

--- frontend/src/lib/components/MilestoneGroup.svelte
+++ frontend/src/lib/components/MilestoneGroup.svelte
@@ -1,14 +1,144 @@
 <script lang="ts">
 	import CardDisplay from '$lib/components/DataDisplay/CardDisplay.svelte';
 	import GalleryDisplay from '$lib/components/DataDisplay/GalleryDisplay.svelte';
-	import { createStyle, getData } from '$lib/components/MilestoneGroup';
+	import { createStyle, surveyData } from '$lib/components/MilestoneGroup';
 	import Breadcrumbs from '$lib/components/Navigation/Breadcrumbs.svelte';
+	import { activeTabChildren } from '$lib/stores/componentStore';
 
-	export let breadcrumbdata: any[] = [];
-	export let data: any[] = [];
-	export let searchData: any[] = [];
+	const data: any[] = surveyData;
 
+	const breadcrumbdata: any[] = [
+		{
+			label: 'Kinderübersicht',
+			onclick: () => {
+				activeTabChildren.update((value) => {
+					return 'childrenGallery';
+				});
+			}
+		},
+		{
+			label: 'Meike',
+			onclick: () => {
+				activeTabChildren.update((value) => {
+					return 'childrenRegistration';
+				});
+			}
+		},
+		{
+			label: 'Bereichsübersicht',
+			onclick: () => {
+				activeTabChildren.update((value) => {
+					return 'milestoneGroup';
+				});
+			}
+		}
+	];
 
+	interface MilestoneData {
+		number: 4;
+		title: string;
+		desc: string;
+		observation: string;
+		help: string;
+		answer: string;
+	}
+
+	interface DataElement {
+		header: string;
+		summary: string;
+		milestoneData: any;
+		progress: number;
+		[key: string]: any;
+	}
+
+	function searchByStatus(data: DataElement[], key: string): DataElement[] {
+		if (key === '') {
+			return data;
+		} else {
+			return data.filter((item) => {
+				// button label contains info about completion status => use for search
+				if (key === 'fertig') {
+					return item.progress === 1;
+				} else if (key === 'unfertig') {
+					return item.progress < 1;
+				} else {
+					return item.header.toLowerCase().includes(key.toLowerCase());
+				}
+			});
+		}
+	}
+
+	function searchBySurvey(data: DataElement[], key: string): DataElement[] {
+		if (key === '') {
+			return data;
+		} else {
+			return data.filter((item) => {
+				return item.header.toLowerCase().includes(key.toLowerCase());
+			});
+		}
+	}
+
+	function searchBySurveyDescription(data: DataElement[], key: string): DataElement[] {
+		if (key === '') {
+			return data;
+		} else {
+			return data.filter((item) => {
+				return item.summary.toLowerCase().includes(key.toLowerCase());
+			});
+		}
+	}
+
+	function searchByMilestone(data: DataElement[], key: string): DataElement[] {
+		if (key === '') {
+			return data;
+		} else {
+			return data.filter((item) => {
+				return item.milestoneData.some((element: MilestoneData) =>
+					element.title.toLowerCase().includes(key.toLowerCase())
+				);
+			});
+		}
+	}
+
+	// README: this is slow and quite a bit of work because a lot of text has to be searched. Kill it?
+	function searchAll(data: DataElement[], key: string): DataElement[] {
+		return [
+			...new Set([
+				...searchBySurvey(data, key),
+				...searchByStatus(data, key),
+				...searchByMilestone(data, key),
+				...searchBySurveyDescription(data, key)
+			])
+		];
+	}
+
+	const searchData: any[] = [
+		{
+			label: 'Alle',
+			placeholder: 'Alle Kategorien durchsuchen',
+			filterFunction: searchAll
+		},
+		{
+			label: 'Bereich',
+			placeholder: 'Nach Beobachtungsbereich suchen',
+			filterFunction: searchBySurvey
+		},
+		{
+			label: 'Bereichsbeschreibung',
+			placeholder: 'Beschreibung von Bereichen durchsuchen',
+			filterFunction: searchBySurveyDescription
+		},
+		{
+			label: 'Meilensteine',
+			placeholder: 'Nach Meilenstein suchen',
+			filterFunction: searchByMilestone
+		},
+		{
+			label: 'Status',
+			placeholder: 'Bereiche nach Status durchsuchen (fertig/unfertig)',
+			filterFunction: searchByStatus
+		}
+	];
 </script>
 
 <div class="flex flex-col border border-gray-200 md:rounded-t-lg dark:border-gray-700">
